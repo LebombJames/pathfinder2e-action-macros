d7dd28e0848c8a777af44708fb26da8afaef6d86
Pro-Dumbass <86080187+Pro-Dumbass@users.noreply.github.com>
2021-06-18 00:07:40 +0100

    Finalised Ver

--- macro-button.js
+++ macro-button.js
@@ -1,24 +1,15 @@
-Hooks.on('renderActorSheet', function(obj, html){
+Hooks.on('renderActorSheet', function(obj, html) {
 
-  // Only inject the link if the actor is of type "character" and the user has permission to update it
-  const actor = obj.actor;
-  // if (!(actor.data.type === "character" && actor.can(game.user, "update"))) return;
+        let element = html.find(".window-header .window-title");
+        if (element.length != 1) return;
 
-  let element = html.find(".window-header .window-title");
-  if (element.length != 1) return;
+        let button = $(`<a class="popout" style><i class="fas fa-book"></i>Add Actions</a>`);
+        button.on('click', () => actionMacroExecute(obj.object));
+        element.after(button);
+    }
 
-  let button = $(`<a class="popout" style><i class="fas fa-book"></i>Action import dialogue</a>`);
-  button.on('click', () => actionMacroExecute(obj.object));
-  element.after(button);
-  }
-   
 );
 
-function actionMacroExecute(targetActor){
-  var MacroIdFinesse = game.macros.getName ("Add Actions Macro").data._id;
-  console.log("Id acquired");
-  console.log(MacroIdFinesse);
-  let actor = targetActor;
-  MacroIdFinesse.execute(actor);
-  console.log("Macro executed")
+function actionMacroExecute(targetActor) {
+    (game.macros.getName("Add Actions Macro")).execute()
 }
