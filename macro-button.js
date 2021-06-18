0c41a5b2525d2556ffb70efa219848ad48170edb
LebombJames <77904738+LebombJames@users.noreply.github.com>
2021-06-18 13:48:48 +0100

    Macro Button Final

--- macro-button.js
+++ macro-button.js
@@ -1,15 +1,19 @@
 Hooks.on('renderActorSheet', function(obj, html) {
-
+		
+		const actor = obj.actor;
+		
+		if (!(actor.data.type === "character" && actor.canUserModify(game.user, "update"))) return;
         let element = html.find(".window-header .window-title");
         if (element.length != 1) return;
 
         let button = $(`<a class="popout" style><i class="fas fa-book"></i>Add Actions</a>`);
-        button.on('click', () => actionMacroExecute(obj.object));
+        button.on('click', () => (actionMacroExecute(obj.object)));
         element.after(button);
     }
 
 );
 
-function actionMacroExecute(targetActor) {
-    (game.macros.getName("Add Actions Macro")).execute()
+async function actionMacroExecute(actor) {
+const macroToRun = await game.packs.filter(p => p.metadata.entity === 'Macro' && p.metadata.package === "pathfinder2eactionmacros")[0].getDocuments()
+await (macroToRun[0]).execute(actor)	
 }
