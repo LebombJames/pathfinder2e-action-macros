a452c9bbd4dc85ac3cf31e21b039865ce89cc815
LebombJames <77904738+LebombJames@users.noreply.github.com>
2022-05-24 01:13:38 +0100

    Show's over folks

--- macro-button.js
+++ macro-button.js
@@ -1,19 +1,12 @@
-Hooks.on('renderActorSheet', function(obj, html) {
-		
-		const actor = obj.actor;
-		
-		if (!(actor.data.type === "character" && actor.canUserModify(game.user, "update"))) return;
-        let element = html.find(".window-header .window-title");
-        if (element.length != 1) return;
-
-        let button = $(`<a class="popout" style><i class="fas fa-book"></i>Add Actions</a>`);
-        button.on('click', () => (actionMacroExecute(obj.object)));
-        element.after(button);
-    }
-
-);
-
-async function actionMacroExecute(actor) {
-const macroToRun = await game.packs.filter(p => p.metadata.entity === 'Macro' && p.metadata.package === "pathfinder2eactionmacros")[0].getDocuments()
-await (macroToRun[0]).execute(actor)	
-}
+Hooks.on('ready', () => {
+    new Dialog({
+        title: "Pathfinder 2e Add Actions",
+        content: `<p>Pathfinder 2e Add Actions is now deprecated. You should unistall the module. </p> <p>It was my first Js project so the code is a mess, plus it just makes your character sheet look messy. You should use <a href=https://foundryvtt.com/packages/pf2e-sheet-skill-actions>PF2e Character Sheet Skill Actions</a> instead, it's much better </p> <p>I love you.</p>`,
+        buttons: {
+            yes: {
+                icon: '<i class="fas fa-heart"></i>'
+            }
+        }
+        
+    }).render(true)
+})
